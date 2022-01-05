01c278b438e2252ecb4e0974e8981c90f23cb83a
Vens <v.kolev@alfatrex.com>
2022-01-05 13:16:02 +0100

    vens fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function bmiRechner(){
     let hoeche = document.getElementById("groesse").value;
     
     if (gewicht == "" || hoeche == ""){
-        document.getElementById("wert").innerHTML = ("Bitte Ihre werte ein.")
+        document.getElementById("wert").innerHTML = ("Ihre Werte, bitte!")
     } else {
     document.getElementById("wert").innerHTML = (gewicht / hoeche / hoeche).toFixed(2);
 }
